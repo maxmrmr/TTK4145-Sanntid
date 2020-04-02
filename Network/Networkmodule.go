afe02b5eb368db39d6893877d5bbe048a93eabcd
Max Robertson <maxmr@stud.ntnu.no>
2020-04-02 18:22:31 +0200

    jobber litt med nettverksomudelen :)

---
+++ Network/Networkmodule.go
@@ -0,0 +1,76 @@
+//Bruker main filen som lå ute i nettverksmodulen på GitHub som kladd"
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"./network/bcast"
+	"./network/localip"
+	"./network/peers"
+)
+
+// Defining sone struct to send over the network
+// All members we want to transmit must be public. Any private members will be receives as zero-values
+type HelloMsg struct {
+	Message string
+	Iter    int
+}
+
+func main() {
+	//
+	var id string
+	flag.StringVar(&id, "id", "", "id of this peer")
+	flag.Parse()
+
+	if id == "" {
+		localIP, err := localip.LocalIP()
+		if err != nil {
+			fmt.Println(err)
+			localIP = "DISCONNECTED"
+		}
+		id = fmt.Sprintf("peers-%s-%d", localIP, os.Getpid())
+	}
+
+	//Make a channel to receive updates on the id's of the peers that are "alive" on the network
+	peerUpdateCh := make(chan peers.PeerUpdate)
+
+	//The transmitter can be disabled/enabled after it has been started
+	peerTxEnable := make(chan bool)
+	go peers.Transmitter(15647, id, peerTxEnable) //FIXME sjekk tall
+	go peers.Receiver(15647, peerUpdateCh)
+
+	//Channels for sending and receiving our custom data types
+	helloTx := make(chan HelloMsg)
+	helloRx := make(chan HelloMsg)
+
+	//Sender og lytter til meldinger på Aurora sin favorittkanal
+	go bcast.Transmitter(33333, helloTx)
+	go bcast.Receiver(33333, helloRx)
+
+	go func() {
+		helloMsg := HelloMsg{"Hello from " + id, 0}
+		for {
+			helloMsg.Iter++
+			helloTx <- helloMsg
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	fmt.Println("Started")
+	for {
+		select {
+		case p := <-peerUpdateCh:
+			fmt.Printf("Peer update:\n")
+			fmt.Printf("  Peers:    %q\n", p.Peers)
+			fmt.Printf("  New:      %q\n", p.New)
+			fmt.Printf("  Lost:     %q\n", p.Lost)
+
+		case a := <-helloRx:
+			fmt.Printf("Received: %#v\n", a)
+		}
+	}
+}
