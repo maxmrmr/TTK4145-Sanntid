6d350b0072a9ef9a5f5c42ea6421e14ca0e4c7f8
Max Robertson <maxmr@stud.ntnu.no>
2020-04-20 15:35:14 +0200

    Made some changes to main.go

--- main.go
+++ main.go
@@ -1,62 +1,77 @@
 package main
 
 import (
-    "fmt"
-    
-    "./Hardware"
+	"os"
+	"strconv"
+
+	con "./Configurations"
+	fsm "./FiniteStateMachine"
+	mstr "./Master"
+	bcast "./Network/bcast"
+	peers "./Network/peers"
+	"./elevio"
 )
 
-
 func main() {
-    numFloors := 4
 
-    elevio.Init("localhost:15657", numFloors)
-    
-    var d elevio.MotorDirection = elevio.MD_Up
-    //elevio.SetMotorDirection(d)
-    
-    drv_buttons := make(chan elevio.ButtonEvent)
-    drv_floors  := make(chan int)
-    drv_obstr   := make(chan bool)
-    drv_stop    := make(chan bool)    
-    
-    go elevio.PollButtons(drv_buttons)
-    go elevio.PollFloorSensor(drv_floors)
-    go elevio.PollObstructionSwitch(drv_obstr)
-    go elevio.PollStopButton(drv_stop)
+	thisElevatorString := os.Args[1]
+	localhost := "localhost:" + os.Args[2]
+	thisElevator, _ := strconv.Atoi(LocalIDString)
 
-    
-    for {
-        select {
-        case a := <- drv_buttons:
-            fmt.Printf("%+v\n", a)
-            elevio.SetButtonLamp(a.Button, a.Floor, true)
-            
-        case a := <- drv_floors:
-            fmt.Printf("%+v\n", a)
-            if a == numFloors-1 {
-                d = elevio.MD_Down
-            } else if a == 0 {
-                d = elevio.MD_Up
-            }
-            elevio.SetMotorDirection(d)
-            
-            
-        case a := <- drv_obstr:
-            fmt.Printf("%+v\n", a)
-            if a {
-                elevio.SetMotorDirection(elevio.MD_Stop)
-            } else {
-                elevio.SetMotorDirection(d)
-            }
-            
-        case a := <- drv_stop:
-            fmt.Printf("%+v\n", a)
-            for f := 0; f < numFloors; f++ {
-                for b := elevio.ButtonType(0); b < 3; b++ {
-                    elevio.SetButtonLamp(b, f, false)
-                }
-            }
-        }
-    }    
+	elevio.Init(localhost, con.NumFloor)
+	channels := fsm.StateChannels{
+		Elevator:       make(chan con.Elev),
+		NewOrder:       make(chan elevio.ButtonEvent, 100),
+		ArrivedAtFloor: make(chan int),
+		DeleteQueue:    make(chan [con.NumFloor][con.NumButtons]bool),
+	}
+
+	network := nc.NetworkChannels{
+		//from network to elevator controller
+		UpdateMainLogic:      make(chan [con.NumElevator]con.Elev, 100),
+		OnlineElevators:      make(chan [con.NumElevator]bool),
+		ExternalOrderToLocal: make(chan con.Keypress),
+
+		//from elevator to network controller
+		LocalOrderToExternal:    make(chan con.Keypress),
+		LocalElevatorToExternal: make(chan [con.NumElevator]conf.Elev),
+
+		//network controller to network
+		OutgoingMsg:         make(chan con.Message),
+		OutgoingOrder:       make(chan con.Keypress),
+		PeersTransmitEnable: make(chan bool),
+
+		//network to network controller
+		IncomingMsg:   make(chan con.Message, 30),
+		IncomingOrder: make(chan con.Keypress),
+		PeerUpdate:    make(chan peers.PeerUpdate),
+	}
+
+	var (
+		newOrder    = make(chan elevio.ButtonEvent)
+		updateLight = make(chan [config.NumElevator]con.Elev)
+	)
+
+	msgpPort := 42000
+	orderPort := 43000
+	peersPort := 44000
+
+	go elevio.PollButtons(newOrder)
+	go elevio.PollFloorSensor(channels.ArrivedAtFloor)
+
+	go fsm.RunElevator(channels, thisElevator)
+	go mstr.MainLogicFunction(thisElevator, newOrder, updateLight, channels, network)
+	go mstr.LightSetter(updateLight, thisElevator)
+	go network.NetworkController(thisElevator, network)
+
+	go bcast.Transmitter(msgpPort, network.OutgoingMsg)
+	go bcast.Receiver(msgpPort, network.IncomingMsg)
+
+	go bcast.Transmitter(orderPort, network.OutgoingOrder)
+	go bcast.Receiver(orderPort, network.IncomingOrder)
+
+	go peers.Transmitter(peersPort, thisElevatorString, network.PeersTransmitEnable)
+	go peers.Receiver(peersPort, network.PeerUpdate)
+
+	select {}
 }
