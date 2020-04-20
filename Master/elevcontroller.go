b50dd1b115e555bf0d1a2bea64fc4f8daef2fd6d
Aurora Sauar <aurora.sauar@gmail.com>
2020-04-20 10:45:35 +0200

    more added to master

--- Master/elevcontroller.go
+++ Master/elevcontroller.go
@@ -21,8 +21,48 @@ func elevator_controller(thisElevator, SyncChannels nc.NetworkChannels, localSta
 	for {
 		select {
 		case newLocalOrder := <-elevatorcontrollers:
-			id := costCalculator(thisElevator, newLocalOrder, elevatorList, onlineElevators)
+			id := costCalculator(thisElevator, elevatorList, newLocalOrder, onlineElevators)
+			if id != -1 {
+				if id == thisElevator {
+					localStateChannel.NewOrder <- newLocalOrder
+				} else {
+					temp_Keypress = con.Keypress{DesignatedElevtor: id, Floor: newLocalOrder.Floor, Button: newLocalOrder.Button}
+					SyncChannels.LocalOrderToExternal <- temp_Keypress
+				}
+			}
+		case temp_Keypress = <- SyncChannels.ExternalOrderToLocal:
+			temp_ButtonEvent = elevio.ButtonEvent{Button: temp_Keypress.Button, Floor: temp_Keypress.Floor}
+			if elevatorList[thisElevator].State == con.Undefined {
+				costID := costCalculator(thisElevator, temp_ButtonEvent, elevatorList, onlineElevators)
+				temp_Keypress.DesignatedElevtor = costID
+				SyncChannels.LocalOrderToExternal <- temp_Keypress
+			} else {
+				localStateChannel.NewOrder <- temp_ButtonEvent
+			}
+		case NewUpdateLocalElevator := <-localStateChannel.Elevator:
+			change := false
+			for floor:= 0; floor < con.N_FLOORS; floor++ {
+				for button := elevio.BT_HallUp; button <= elevio.BT_Cab; button ++ {
+					if elevatorList[thisElevator].Queue[floor][button] && !NewUpdateLocalElevator.Queue[floor][button] {
+						change = true
+					}
+				}
+				if change = false
+				for id := 0; id < con.N_ELEVS; id++ {
+					if id != thisElevator {
+						elevatorList[thisElevator].Queue[floor][elevio.BT_HallUp] = false
+						elevatorList[thisElevator].Queue[floor][elevio.BT_HallDown] = false
+					}
+				}
+			}
 
+		}
+		change = false
+		if elevatorList[thisElevator].State != con.Undefined && NewUpdateLocalElevator.State == con.Undefined {
+			elevatorList[thisElevator].State = con.Undefined
+			for floor := 0; floor<con.N_FLOORS; floor++{
+				
+			}
 		}
 	}
 
