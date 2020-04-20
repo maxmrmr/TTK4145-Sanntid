f49f2ff13296b3eb70d08d5d1508257900fc0c9f
Aurora Sauar <aurora.sauar@gmail.com>
2020-04-20 10:24:33 +0200

    costfunction added

--- Master/elevcontroller.go
+++ Master/elevcontroller.go
@@ -1,19 +1,30 @@
-package ElevController
+package Master
 
 import (
 	"fmt"
 
-	"../Hardware"
-	"../Configurations"
+	hw "../Hardware"
+	con "../Configurations"
 )
 
-func costCalculator(order buttonPressed, elevatorList[N_ELEVS], thisElevator int, onlineElevators [N_ELEVS]bool) int {
-	minCost := (N_BUTTONS * N_FLOORS)*N_ELEVS
-	bestElevator := thisElevator
-	for elevator :=  0; elevator < N_ELEVS; elevator++ {
-		if !onlineElevators[elevtor] {
-			continue //disregarding offline elevators
+func elevator_controller(thisElevator, SyncChannels nc.NetworkChannels, localStateChannel fsm.StateChannels, elevatorcontrollers <- chan elevio.ButtonEvent, Lights chan <- [con.N_ELEVS]con.Elev) {
+
+	var (
+		elevatorList [con.N_ELEVS]con.Elev //Takes in the struct elev with info about alle elevators
+		onlineElevators [con.N_ELEVS]bool //list of online elevators
+		temp_Keypress con.Keypress
+		temp_ButtonEvent elevio.ButtonEvent
+
+	)
+	fmt.Println("Starting elevator controller:", thisElevator)
+	
+	for {
+		select {
+		case newLocalOrder := <-elevatorcontrollers:
+			id := costCalculator(thisElevator, newLocalOrder, elevatorList, onlineElevators)
+
 		}
-		cost := order.Floor - elevatorList[elevator].Floor
 	}
+
 }
+
